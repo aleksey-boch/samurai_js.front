dc1c34495c7b41a9b2f8943953e67fe298bff6c2
OlexiiB <bochaleksey@gmail.com>
2023-03-20 08:29:58 +0100

    08

--- src/api/api.ts
+++ src/api/api.ts
@@ -87,7 +87,7 @@ export const authAPI = {
         return instance.get<MeResponseType>(`auth/me`)
             .then(res => res.data);
     },
-    login(email: string, password: string, rememberMe = false, captcha: null | string = null) {
+    login(email: string, password: string, rememberMe: boolean = false, captcha: null | string = null) {
         return instance.post<LoginResponseType>(`auth/login`, {email, password, rememberMe, captcha})
             .then(res => res.data);
     },
